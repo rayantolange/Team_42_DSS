7420385e6a81c8b1f66764b3392d1646c197adf4
rayantolange <tolangerayan@gmail.com>
2026-08-02 11:10:35 +0545

    Add Vercel frontend URL to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://team-42-dss.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
